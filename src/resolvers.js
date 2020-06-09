19b0b510524361bde448becde58b188048e0a503
Kirill Korolyov <kirill.korolyov@gmail.com>
2020-06-09 23:07:16 +0100

    Live coding stage 1

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,10 +1,40 @@
 const { query, sql } = require("./database");
 const { orderByKeys, orderAndGroupByKeys, onNextTick } = require("./utils");
 
+async function fetchArtistsByIDs(artistIDs) {
+  const artists = await query(
+    sql`select * from artists where id = ANY(${artistIDs})`
+  );
+
+  return orderByKeys(artists, artistIDs, (a) => a.id);
+}
+
+async function fetchTracksByAlumIDs(albumIDs) {
+  const tracks = await query(
+    sql`select * from tracks where album_id = ANY(${albumIDs}) ORDER BY index`
+  );
+
+  return orderAndGroupByKeys(tracks, albumIDs, (t) => t.album_id);
+}
+
 exports.resolvers = {
   Query: {
     albums: async () => {
-      return [];
+      const albums = await query(sql`select * from albums`);
+
+      const artistIDs = albums.map(({ artist_id }) => artist_id);
+      const artistsById = await fetchArtistsByIDs(artistIDs);
+      albums.forEach((album, index) => {
+        album.artist = artistsById[index];
+      });
+
+      const albumIDs = albums.map(({ id }) => id);
+      const tracksByAlbumId = await fetchTracksByAlumIDs(albumIDs);
+      albums.forEach((album, index) => {
+        album.tracks = tracksByAlbumId[index];
+      });
+
+      return albums;
     },
   },
 };
